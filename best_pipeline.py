2327567cff6cb5465d01a7965367a1802c17c203
Armand Morin <amorin@tower-research.com>
2024-09-12 09:54:25 -0400

    amorin push
    Change-Id: If4d2809909580c300a3e6c74aca5c69aca15d3d1

--- best_pipeline.py
+++ best_pipeline.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.tree import DecisionTreeRegressor
-from tpot.builtins import StackingEstimator
-from tpot.export_utils import set_param_recursive
+from xgboost import XGBRegressor
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,13 +9,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -0.7488422872559023
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=DecisionTreeRegressor(max_depth=10, min_samples_leaf=17, min_samples_split=12)),
-    DecisionTreeRegressor(max_depth=10, min_samples_leaf=3, min_samples_split=4)
-)
-# Fix random state for all the steps in exported pipeline
-set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+# Average CV score on the training set was: -0.026353731498777594
+exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=6, min_child_weight=8, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=1.0, verbosity=0)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
